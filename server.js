9352eeee9596638ec1654b595edf1b191e6ea6f7
Chico <chico.charlesworth@googlemail.com>
2019-04-16 08:05:42 +0100

    Return json ok response for GET /

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.get('/', function(req, res){
+  res.json({"ok": true});
+});
+
 const port = process.env.PORT || 3001
 http.listen(port, function(){
   console.log('listening on *:' + port);
